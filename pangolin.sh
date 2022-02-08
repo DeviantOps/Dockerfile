0d632855c9dcf9c0c7c564ccc815ec776e800e83
DeviantOps <98871581+DeviantOps@users.noreply.github.com>
2022-02-08 16:47:15 +0100

    Update pangolin.sh

--- pangolin.sh
+++ pangolin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd pangolin 
-conda env create -f environment.yml 
-conda activate pangolin 
-#yes | pip install
-#
+conda env create -f environment.yml
+#conda activate pangolin 
+yes | pip install .
+rm -rf /root/.cache/pip
+pangolin --version &> /pangolin-version.txt
