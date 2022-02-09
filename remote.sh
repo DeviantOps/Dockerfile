f7193f4e7f3fbb914116cc49481cb32f2cc9d5f3
DeviantOps <98871581+DeviantOps@users.noreply.github.com>
2022-02-09 16:34:04 +0100

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -5,5 +5,7 @@ service redis-server start
 #
 useradd -m -p yK9SGBCKF5AMI malcolm
 usermod -aG sudo malcolm
+# start Jupyter
+nohup jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root &
 # spawn a shell
 /bin/bash
